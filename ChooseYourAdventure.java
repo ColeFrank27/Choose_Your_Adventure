f1edd444dde03a08c36b0f39dd2c500e025e199f
ColeFrank <cole.frank27@student.tamdistrict.org>
2024-10-27 19:45:27 -0700

    Final project

--- ChooseYourAdventure.java
+++ ChooseYourAdventure.java
@@ -15,6 +15,29 @@ public class ChooseYourAdventure {
         if (choiceOne.equals("1")) {
             System.out.print("You over slept 30 more minutes. ");
             timeToSchool += 30;
+            System.out.println(
+                    "Do you want to pack a lunch or eat school lunch? Option 1: pack a lunch. Option 2: Eat school lunch. ");
+            String option1 = userInput.nextLine();
+
+            if (option1.equals("1")) {
+                timeToSchool += 15;
+                System.out.println("You packed yourself a great lunch.");
+
+            } else {
+                System.out.println(" You settled for the school lunch");
+
+            }
+            System.out.println(
+                    "Its time to leave for school in your car. Option 1: speed to make it on time. Option 2: Drive normally and be safe");
+            String option3 = userInput.nextLine();
+
+            if (option3.equals("1")) {
+                System.out.println("You crashed and had to walk to school");
+                timeToSchool += 100;
+            } else {
+                System.out.println("You arrived at school alive");
+                timeToSchool += 30;
+            }
 
         } else {
             System.out.println("Good choice. School starts in 1 hour and 15 minutes. You have time to eat breakfast.");
